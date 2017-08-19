d356630c3e9590eb66c2d8e7a1634ea255f9dd77
Mmis1000 <mmis1000@yahoo.com.tw>
2017-08-19 15:05:32 +0800

    refactor and add inline download

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-var config = require('./config.json');
+const path = require("path");
+const storage = require("node-persist");
+const BSON = require('bson')
+const config = require('./config.json');
+const bson = new BSON();
+const downloadFolderPath = path.resolve(__dirname, config.path);
+
+// init storage before everything up
+storage.initSync({
+	dir: path.resolve(downloadFolderPath, '.info'),
+	stringify: bson.serialize.bind(bson),
+	parse: function (buf) {
+	    return bson.deserialize(buf, {promoteBuffers: true});
+	},
+	encoding: null,
+	continuous: true, 
+    forgiveParseErrors: false // [NEW]
+});
+
 var bot = require("./lib/bot");
 var app = require("./lib/server");
 
